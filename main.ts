cba3c29e35f19f8742388214f7075382d950f605
hananinas <hanani.nas@gmail.com>
2024-02-26 16:47:04 +0100

    Update main.ts and delete routes/index.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,11 @@ import { Hono } from 'https://deno.land/x/hono@v4.0.7/mod.ts'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello IBM SL')
+})
+
+app.get('/api', (c) => {
+  return c.text('Here the api will live')
 })
 
 Deno.serve(app.fetch)
