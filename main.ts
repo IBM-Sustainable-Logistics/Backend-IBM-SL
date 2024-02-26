c9dff5229ce3acbb093d5d5895607c44bc81ba4f
hananinas <hanani.nas@gmail.com>
2024-02-26 12:31:55 +0100

    Update Oak and Cors dependencies, add API routes

--- main.ts
+++ main.ts
@@ -1,9 +1,21 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Welcome to IBM-SL Api";
+  })
+  .get("/api", (context) => {
+    console.log("coming soon")
+  })
+  .get("/api/", (context) => {
+    console.log("coming soon")
+  });
 
 const app = new Application();
-
-app.use((ctx) => {
-  ctx.response.body = "Hello world!";
-});
+app.use(oakCors()); // Enable CORS for All Routes
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 await app.listen({ port: 8000 });
