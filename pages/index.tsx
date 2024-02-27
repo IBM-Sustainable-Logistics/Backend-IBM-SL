4681b4f4a542af4a901c28349876281aaa48a68b
hananinas <hanani.nas@gmail.com>
2024-02-27 12:20:33 +0100

    Add meta tags and title to HTML head

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,12 @@ import { app } from "../main.ts"
 export const Layout: FC = (props) => {
   return (
     <html>
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>IBM SL </title>
+    </head>
       <body>{props.children}</body>
     </html>
   )
