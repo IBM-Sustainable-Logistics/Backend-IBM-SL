c09e1b3a22249ee81a3da9f59838911f54bcd299
marc9622 <maaw@itu.dk>
2024-02-28 12:45:49 +0100

    initial open-api impl

--- main.ts
+++ main.ts
@@ -3,20 +3,25 @@
 import { Hono } from 'https://deno.land/x/hono@v4.0.7/mod.ts'
 import { Index } from "./pages/index.tsx";
 import { serveStatic } from "https://deno.land/x/hono@v4.0.7/middleware.ts";
+import { z, OpenAPIHono } from "npm:@hono/zod-openapi@0.9.5";
 
-export const app = new Hono()
-
+export const app = new OpenAPIHono();
 
 // serve static files here 
-app.use('/logo.svg', serveStatic({ path: './assets/ibm-logo.svg' }))
+//app.use('/logo.svg', serveStatic({ path: './assets/ibm-logo.svg' }))
 
 
 // static get pages using jsx here 
 Index()
 
+const api = new Hono().basePath('/api')
 
 // api routes get/put 
-app.get('/api', (c) => c.text('Here the api will live'))
+api.get('/', async (c) => {
+    const body = await c.req.json()
+
+    c.text('Here the api will live')
+})
 
 
 
