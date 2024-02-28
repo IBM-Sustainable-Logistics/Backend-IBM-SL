58869f5e0ad538fbee0392e8cf78e1159a9f4ac5
hananinas <hanani.nas@gmail.com>
2024-02-28 09:51:23 +0100

    Add IBM logo and update README and index page

--- main.ts
+++ main.ts
@@ -2,8 +2,13 @@
 
 import { Hono } from 'https://deno.land/x/hono@v4.0.7/mod.ts'
 import { Index } from "./pages/index.tsx";
+import { serveStatic } from "https://deno.land/x/hono@v4.0.7/middleware.ts";
 
 export const app = new Hono()
+
+
+// serve static files here 
+app.use('/logo.svg', serveStatic({ path: './assets/ibm-logo.svg' }))
 
 
 // static get pages using jsx here 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,44 @@
 
 import { FC } from "https://deno.land/x/hono@v4.0.7/middleware.ts"
 import { jsx, Fragment } from "https://deno.land/x/hono@v4.0.7/middleware.ts"
+import { css, cx, keyframes, Style } from 'https://deno.land/x/hono/helper.ts'
 import { app } from "../main.ts"
+
+
+const background = css `
+  background: #edf5ff;
+`
+
+const headerClass = css`
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-weight: normal;
+  color: #42be65;
+  padding: 1rem;
+`
+
+const button = css `
+  background-color: #0f62fe;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+
+`
+
+const flex = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
+  padding-top: 50px;
+`  
+
+const logo = css`
+  width: 200px;
+`
 
 export const Layout: FC = (props) => {
   return (
@@ -12,9 +49,10 @@ export const Layout: FC = (props) => {
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <title>IBM SL </title>
+      <Style />
+      <title >IBM SL</title>
     </head>
-      <body>{props.children}</body>
+      <body class={background}>{props.children}</body>
     </html>
   )
 }
@@ -22,7 +60,12 @@ export const Layout: FC = (props) => {
 export const Top: FC = () => {
   return (
     <Layout>
-      <h1>Welcome to IBM SL</h1>
+      <div class={flex}>
+          <img class={logo} src="/logo.svg"></img>
+         <h1 class={headerClass} >Welcome to IBM SL</h1>
+         <a href="https://ibm-sl.deno.dev/"><button class={button}> Read more </button></a>
+      </div>
+
     </Layout>
   )
 }
