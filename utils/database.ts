4eb0b63665f3b76042d08a03acce0a172fb5e90c
hananinas <hanani.nas@gmail.com>
2024-03-05 14:54:52 +0100

    Remove unused imports and code

--- utils/database.ts
+++ utils/database.ts
@@ -1,8 +1,1 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-import { load } from "https://deno.land/std@0.218.0/dotenv/mod.ts";
-
-const env = await load();
-
-export const supabase = createClient(env.DATABASE_URL, env.ANON)
-
 
